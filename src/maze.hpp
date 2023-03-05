efa98f6cc006c8f42c00bacd4aa7a0fe95284363
itsmaxymoo <max.loiacono@protonmail.com>
2023-03-04 22:29:23 -0500

    assign char values to MazeTile

--- src/maze.hpp
+++ src/maze.hpp
@@ -12,7 +12,7 @@ This file defines a Maze/Maze Generator class.
 
 namespace TCODMaze {
 
-enum MazeTile { FLOOR, WALL, START, FINISH };
+enum MazeTile { FLOOR = ' ', WALL = '#', START = 'S', FINISH = 'F' };
 
 class Maze {
  private:
