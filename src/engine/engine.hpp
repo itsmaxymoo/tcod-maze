9a07d4b8d2e376de31904cecf0f75e13337c7bdb
itsmaxymoo <max.loiacono@protonmail.com>
2023-04-03 14:47:23 -0400

    engine include guard

--- src/engine/engine.hpp
+++ src/engine/engine.hpp
@@ -4,6 +4,9 @@ This class defines the main game loop.
 
 */
 
+#ifndef TCOD_MAZE_ENGINE
+#define TCOD_MAZE_FRONTEND
+
 namespace TCODMaze {
 
 #include <memory>
@@ -11,16 +14,19 @@ namespace TCODMaze {
 #include "actor.hpp"
 #include "scene.hpp"
 
-enum GameState { PRE, MAIN, LOSE };
+    enum GameState { PRE, MAIN, LOSE };
 
-class Engine {
- private:
-  GameState state;
+    class Engine {
+    private:
+        GameState state;
 
- public:
-  std::shared_ptr<Scene> scene;
-  std::weak_ptr<Actor> player;
+    public:
+        std::shared_ptr<Scene> scene;
+        std::weak_ptr<Actor> player;
 
-  GameState update();
-};
+        GameState update();
+    };
 }  // namespace TCODMaze
+
+
+#endif
