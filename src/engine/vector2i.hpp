e6ef0a05c3ac8b0a876fd58b69bc05677424d96f
itsmaxymoo <max.loiacono@protonmail.com>
2023-05-26 18:20:38 -0400

    better hashing function

--- src/engine/vector2i.hpp
+++ src/engine/vector2i.hpp
@@ -7,14 +7,15 @@ This file defines a Vector2D object
 #ifndef TCOD_MAZE_VECTOR2
 #define TCOD_MAZE_VECTOR2
 
+#include <cstdint>
 #include <functional>
 
 namespace TCODMaze {
 
 struct vector2i {
-  int x = 0, y = 0;
+  int32_t x = 0, y = 0;
   vector2i();
-  vector2i(int x, int y);
+  vector2i(uint32_t x, uint32_t y);
   bool operator==(const vector2i &that) const;
   vector2i operator+(const vector2i &that);
   vector2i operator-(const vector2i &that);
@@ -24,7 +25,8 @@ struct vector2i {
 
 struct vector2i_hash {
   int operator()(const vector2i &t) const {
-    return std::hash<int>()(t.x) ^ std::hash<int>()(t.y);
+    int64_t xl = (int64_t)t.x + INT32_MAX, yl = (int64_t)t.y + INT32_MAX;
+    return std::hash<int>()(xl) ^ std::hash<int>()(yl);
   }
 };
 
