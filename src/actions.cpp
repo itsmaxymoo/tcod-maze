100d71bb75a698ef7b7dfca551263c5ce26bd543
itsmaxymoo <max.loiacono@protonmail.com>
2023-05-27 13:16:16 -0400

    move action

---
+++ src/actions.cpp
@@ -0,0 +1,22 @@
+/*
+
+Custom actions implementation
+
+*/
+
+#include "actions.hpp"
+
+#include "engine/actor.hpp"
+#include "engine/scene.hpp"
+
+move_action::move_action(const TCODMaze::vector2i& dir) {
+  this->direction = dir;
+}
+
+void move_action::execute(TCODMaze::actor* subject) {
+  TCODMaze::vector2i new_pos = subject->position + this->direction;
+
+  if (!(subject->get_parent_scene()->get_cell(new_pos).collision)) {
+    subject->position = new_pos;
+  }
+}
---
+++ src/actions.hpp
@@ -0,0 +1,23 @@
+/*
+
+Action definitions for our game
+
+*/
+
+#ifndef TCOD_MAZE_CUSTOM_ACTIONS
+#define TCOD_MAZE_CUSTOM_ACTIONS
+
+#include "engine/action.hpp"
+#include "engine/actor.hpp"
+#include "engine/vector2i.hpp"
+
+class move_action : public TCODMaze::action {
+ private:
+  TCODMaze::vector2i direction;
+
+ public:
+  move_action(const TCODMaze::vector2i&);
+  void execute(TCODMaze::actor*);
+};
+
+#endif
