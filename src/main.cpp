3a0f8c3cd988d540e9d711467f2b752117b7c54f
itsmaxymoo <max.loiacono@protonmail.com>
2023-05-26 23:22:37 -0400

    remove test code

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,8 @@
 
 #include "engine/engine.hpp"
 #include "tcod_frontend.hpp"
-#include "engine/maze.hpp"
-#include "engine/tile.hpp"
 
 int main() {
-  TCODMaze::tile_t fin = TCODMaze::TILES::FINISH;
-  TCODMaze::tile_t fin2 = fin;
-
   TCODMaze::Engine engine;
   TCODAsciiFrontEnd frontend;
 
